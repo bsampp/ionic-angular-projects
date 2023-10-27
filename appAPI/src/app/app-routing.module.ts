4a4dd6b4fcae3ec6b1eae1210ea849cae9354dae
OtavioPadzz <tatopadovam@hotmail.com>
2023-10-27 10:29:33 -0300

    aula1

---
+++ appAPI/src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'movies',
+    loadChildren: () => import('./view/movies/movies.module').then( m => m.MoviesPageModule)
+  },
+  {
+    path: 'movie-detail',
+    loadChildren: () => import('./view/movie-detail/movie-detail.module').then( m => m.MovieDetailPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
